7a92c36505e5646d50fdffba67fd758bf6d115e9
nickycarr81 <nickycarr81@hotmail.com>
2018-04-07 13:09:43 +0100

    Update B00216319_CourseWork.ino

--- B00216319_CourseWork.ino
+++ B00216319_CourseWork.ino
@@ -1,9 +1,12 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>                // WiFi Library
+#include <SimpleDHT.h>                  // Digital Temp Library
 
-const char* ssid = "**********";       // This is the SSID of the network;
-const char* password = "********";    // This is the password for the network;
-
+const char* ssid = "";       // This is the SSID of the network; "BTHub4-NKRC";
+const char* password = "";    // This is the password for the network; "c272e73d5b";
 WiFiServer server(80);                  // This is the service port being used
+
+int pinTemp1 = 16;
+SimpleDHT11 dht11;
 
 void setup() {
   Serial.begin(115200);                 
@@ -36,11 +39,35 @@ void setup() {
     Serial.println("WeMos D1 Server started");
     Serial.print("Allocation of IP address: ");
     Serial.println(WiFi.localIP());
-   
+    Serial.println("");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //Serial.println("Commence Temperature Readings");
+  
+  byte temp1 = 0;
+  byte humidity = 0;
+  byte data[40] = {0};
+  
+  if (dht11.read(pinTemp1, &temp1, &humidity, data)) {
+    Serial.print("Read DHTll failed");
+    return;
+  }
+
+ /* Serial.print("Temp 1: ");
+  for (int i=0; i<40; i++){
+    Serial.print((int)data[i]);
+    if (i>0 && ((i+1) % 4) ==0) {
+      Serial.print(' ');
+    }
+  }*/
+  Serial.println("");
+  Serial.print("Temparature 1: ");
+  Serial.print((int)temp1); Serial.print(" *C, ");
+  delay(3000);
+
+  
 
 }

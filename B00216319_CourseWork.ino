86a8cb4d289a22ce1cb0c34792000c4defbb7b3c
nickycarr81 <nickycarr81@hotmail.com>
2018-04-09 20:44:01 +0100

    Update B00216319_CourseWork.ino

--- B00216319_CourseWork.ino
+++ B00216319_CourseWork.ino
@@ -7,6 +7,11 @@ const char* ssid = "BTHub4-NKRC";       // This is the SSID of the network; "BTH
 const char* password = "c272e73d5b";    // This is the password for the network; "c272e73d5b";
 WiFiServer server(80);                  // This is the service port being used
 
+int ledPin = 14;    // LED Pin - (5 Uno)
+int buttonApin = 0;   // LED Pin - (9 Uno)
+int buttonBpin = 13;   // LED Pin - (8 Uno)
+byte leds = 0;
+
 int pinTemp1 = 16;
 int pinTemp2 = 5;
 SimpleDHT11 dht11;
@@ -15,17 +20,21 @@ SimpleDHT11 dht112;
 
 #define RST_PIN   2     // RST Pin 2 - (9 Uno)
 #define SS_PIN   15     // SS Pin 15 - (10 Uno)
-
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
-
 #define NEW_UID {0xDE, 0xAD, 0xBE, 0xEF}
 MFRC522::MIFARE_Key key;
+
+
 
 void setup() {
   Serial.begin(115200);                 
   delay(20);                            // Delay in miliseconds between reads
   SPI.begin();                          // Start Init SPI bus
   mfrc522.PCD_Init();                   // Init MFRC522 card
+
+    pinMode(ledPin, OUTPUT);
+    pinMode(buttonApin, INPUT_PULLUP);  
+    pinMode(buttonBpin, INPUT_PULLUP);
 
   /**
    * Connecting to the WiFi network
@@ -56,10 +65,15 @@ void setup() {
     Serial.println(WiFi.localIP());
     Serial.println("");
 
+
+    
+
 }
 
 void loop() {
-  
+
+
+
   //Serial.println("Commence Temperature Readings");
   
   byte temp1 = 0;
@@ -75,6 +89,9 @@ void loop() {
   Serial.println("");
   Serial.print("Equipment Room: ");
   Serial.print((int)temp1); Serial.print(" *C, ");
+  Serial.println("");
+  delay(3000);
+
 
   byte temp2 = 0;
   byte humidity2 = 0;
@@ -89,7 +106,16 @@ void loop() {
   Serial.print((int)temp2); Serial.print(" *C, ");
   delay(3000);
 
-   // Look for new cards, and select one if present
+  if ((int)temp1 == (int)temp2)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  if ((int)temp1 > (int)temp2)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+
+      // Look for new cards, and select one if present
   if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
     delay(50);
     return;
@@ -103,5 +129,9 @@ void loop() {
   } 
 
   delay(2000);
+
+
+
+
 
 }
